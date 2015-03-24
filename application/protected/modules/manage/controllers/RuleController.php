ade36341463c1be4449d549e40250b25ffaa5dae
xiaomlove <353856593@qq.com>
2015-03-24 10:40:31 +0800

    fixed sort

--- application/protected/modules/manage/controllers/RuleController.php
+++ application/protected/modules/manage/controllers/RuleController.php
@@ -27,6 +27,10 @@ class RuleController extends CommonController
 		}
 		elseif (App::ins()->request->isPost())
 		{
+			if (empty($_POST['id']) || !ctype_digit($_POST['id']))
+			{
+				$this->goError();
+			}
 			$this->submit($model, $_POST, 'update');
 		}
 	}
@@ -50,11 +54,21 @@ class RuleController extends CommonController
 	{
 		if ($model->validate($data))
 		{
-			if ($data['parent_id'] > 0)
+			if ($data['parent_id'] > 0)//非一级权限，sort要跟其父权限的sort一致
 			{
-				$sort = $data['sort'];
-				unset($data['sort']);
+				$parent = $model->findByPk($data['parent_id']);
+				if (empty($parent))
+				{
+					$model->setError('parent_id', '父权限不存在');
+					goto A;
+				}
+				$data['sort'] = $parent['sort'];
 			}
+			else 
+			{
+				$isParent = TRUE;
+			}
+			
 			if ($action === 'insert')
 			{
 				$result = $model->insert($data);
@@ -65,20 +79,53 @@ class RuleController extends CommonController
 				$result = $model->updateByPk($data['id'], $data);
 				$id = $data['id'];
 			}
-			if ($result >= 0)
+			
+			if ($result !== FALSE)
 			{
-				if (isset($sort) && $id)
+				if ($result == 0)
 				{
-					$sql = "UPDATE rule SET sort=$sort WHERE path LIKE concat((SELECT path FROM rule WHERE id=$id),%)";
+					//没有变化，不用更新
+					goto G;
+				}
+				if (isset($isParent) && $isParent)
+				{
+					//更新path及level，一级权限path为其自身id
+					$updatePathLevel = $model->updateByPk($id, array('path' => $id, 'level' => 1));
+					if ($updatePathLevel === FALSE)
+					{
+						$model->setError('parent_id', '更新一级权限path&level失败');
+						goto A;
+					}
+					//当它为一级权限，改变了其sort其后代也跟着变
+					$sql = "UPDATE rule SET sort={$data['sort']} WHERE path LIKE '{$id}%'";
+					//$sql = "UPDATE rule SET sort={$data['sort']} WHERE path LIKE concat((SELECT path FROM rule WHERE id=$id),%)";
 					$updateSort = $model->execute($sql);
 					if ($updateSort === FALSE)
 					{
 						trigger_error('更新sort失败', E_USER_ERROR);exit;
 					}
 				}
+				else
+				{
+					//非一级权限，sort不会被改变，只需要更新path及level
+					$updatePathLevel = $model->updateByPk($id, array('path' => $parent['path'].','.$id, 'level' => $parent['level']+1));
+					if ($updatePathLevel === FALSE)
+					{
+						$model->setError('parent_id', '更新非一级权限path&level失败');
+						goto A;
+					}
+				}
+				G:
+//				exit('stop redirect');
+				
 				$this->redirect('manage/rule/list');
 			}
+			else 
+			{
+				$model->setError('name', 'submit执行失败');
+			}
 		}
+		A:
 		$model->setData($data);
 		$html = $this->render('ruleform', array('model' => $model));
 		echo $html;
@@ -102,7 +149,7 @@ class RuleController extends CommonController
 		{
 			$sql = "SELECT * FROM rule";
 		}
-		$sql .= " ORDER BY sort ASC";		
+		$sql .= " ORDER BY sort ASC,path ASC";		
 		$parentList = $model->findBySql($sql);
 // 		echo '<pre/>';
 // 		var_dump($parentList);exit();
@@ -113,7 +160,7 @@ class RuleController extends CommonController
 			foreach ($parentList as $rule)
 			{
 				$selected = "";
-				if ($id == $rule['id'])
+				if ($parentId == $rule['id'])
 				{
 					$selected = " selected";
 				}
