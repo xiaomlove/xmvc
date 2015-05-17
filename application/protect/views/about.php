dc9b2dce64f6e57604cd3199f15c1ab233529dcc
xiaomlove <353856593@qq.com>
2015-05-17 17:36:53 +0800

    update about a login

--- application/protect/views/about.php
+++ application/protect/views/about.php
@@ -11,10 +11,10 @@ body{padding-bottom: 100px}
 			这只是一个新手练习的作品，还有诸多的不完善。渴望得到你的帮助，让我们一起来完善它。欢迎提出意见与建议，可到论坛进行回帖或发帖。
 		</p>
 		<p>
-			网站主要借鉴自浙江大学团队的<a href="http://sourceforge.net/projects/nexusphp/">NexusPHP</a>，在此先感谢前辈们的付出。近来听高人指点，国外有个<a href="http://whatcd.github.io/Gazelle/">Project Gazelle</a>已经很完善。一睹芳容发现确实很多国外站以及韩国站都是用此搭建。但个人还是习惯NexusPHP的功能与界面。
+			网站主要借鉴自浙江大学团队的<a href="http://sourceforge.net/projects/nexusphp/" target="_blank">NexusPHP</a>，在此先感谢前辈们的付出。近来听高人指点，国外有个<a href="http://whatcd.github.io/Gazelle/" target="_blank">Project Gazelle</a>已经很完善。一睹芳容发现确实很多国外站以及韩国站都是用此搭建。但个人还是习惯NexusPHP的功能与界面。
 		</p>
 		<p>
-			网站采用<a href="http://php.net/" target="_blank">PHP</a>为后台开发语言，<a href="http://www.mysql.com/">Mysql</a>数据库，前端页面是<a href="http://getbootstrap.com/" target="_blank">Bootstrap</a>框架，并使用了<a href="http://jquery.com/" target="_blank">jQuery</a>工具库。PHP的书写风格借鉴了国外<a href="http://www.yiiframework.com/" target="_blank">Yii Framework</a>框架与国内<a href="http://www.thinkphp.cn/" target="_blank">Think PHP</a>框架。
+			网站采用<a href="http://php.net/" target="_blank">PHP</a>为后台开发语言，<a href="http://www.mysql.com/" target="_blank">Mysql</a>数据库，前端页面是<a href="http://getbootstrap.com/" target="_blank">Bootstrap</a>框架，并使用了<a href="http://jquery.com/" target="_blank">jQuery</a>工具库。PHP的书写风格借鉴了国外<a href="http://www.yiiframework.com/" target="_blank">Yii Framework</a>框架与国内<a href="http://www.thinkphp.cn/" target="_blank">Think PHP</a>框架。
 		</p>
 		<p>
 			联系作者：微博-@chenyulingxi。
